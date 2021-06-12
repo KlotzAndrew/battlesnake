ebe76885badfbb79d685f11b367313cb1b1963ce
Andrew Klotz <agc.klotz@gmail.com>
2021-06-12 03:12:38 +0000

    switch to nightly and add routes
    Signed-off-by: Andrew Klotz <agc.klotz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-#[macro_use] extern crate rocket;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::http::Status;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[post("/start")]
+fn start() -> Status {
+    Status::Ok
+}
+
+#[post("/move")]
+fn movement() -> Status {
+    Status::Ok
+}
+
+#[post("/end")]
+fn end() -> Status {
+    Status::Ok
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, start, movement, end]).launch();
 }
